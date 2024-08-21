9bbcca6bc88e3a420c26af96ccca734aeec192c2
AdmiN <AdmiN@ITWS0025>
2024-08-21 15:17:56 +0530

    Login page

--- src/devops/Logging.java
+++ src/devops/Logging.java
@@ -1,10 +1,69 @@
 package devops;
 
+
+
+import java.util.Scanner;
+
+
+
 public class Logging {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
+
+    
+
+    private static final String USERNAME = "admin";
+
+    private static final String PASSWORD = "password123";
+
+
+
+    public static void main(String[] args) {
+
+        
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+       
+
+        System.out.print("Enter username: ");
+
+        String inputUsername = scanner.nextLine();
+
+
+
+        
+
+        System.out.print("Enter password: ");
+
+        String inputPassword = scanner.nextLine();
+
+
+
+        
+
+        if (USERNAME.equals(inputUsername) && PASSWORD.equals(inputPassword))
+
+        {
+
+            System.out.println("Sign-in successful!");
+
+        } 
+
+        else 
+
+        {
+
+            System.out.println("Invalid username or password.");
+
+        }
+
+
+
+        scanner.close();
+
+    }
 
 }
